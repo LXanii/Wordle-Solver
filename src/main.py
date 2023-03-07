79589e80a55bf81fd2a69557d086dcde7a92f572
Xanii <73562093+LXanii@users.noreply.github.com>
2023-03-07 10:51:02 -0500

    more optimizations

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 dictionary = [] # used in load dict
-potential_word = [] # used in look_For_word
 temp_letters = [] #variable to store our list w
 
 def load_dict(): # made by joe | loads the dictionary to a list
@@ -8,29 +7,27 @@ def load_dict(): # made by joe | loads the dictionary to a list
     split_dic = dic.read() # reads the opened file
     split_dic = split_dic.split("\n") # splits every new line
     for i in split_dic: # checks for every word
-        dictionary.append(i.lower()) # adds the word to the dictionary array
-        potential_word.append(i.lower())
+        if len(i) == 5:
+            dictionary.append(i.lower()) # adds the word to the dictionary array
     print("Finished loading Dictionary\n" + str(len(dictionary)), "words found.")
 
 def look_for_word(a, b, c, d, e): # made by joe | gets the index values of the letters
     matching = []
-    to_find = [] # empty list to add the indexes to
-    to_find.clear() # clears the list
     matching.clear()
     look_for = a + b + c + d + e # adds the letters together
     print("This may take a little...")
     print("Checking for", look_for)
     for i in range(1, len(look_for) + 1): # loops for the amount of indexes
         if look_for[i-1] != ".": # checks if the placement if valid
-            for p in potential_word:
+            for p in dictionary:
                 if p[i-1] == look_for[i-1]:
                     matching.append(p)
-            potential_word.clear()
+            dictionary.clear()
             for m in matching:
-                potential_word.append(m)
+                dictionary.append(m)
             matching.clear()
 
-    print("Search Term", look_for, "\n" + str(potential_word))
+    print("Search Term", look_for, "\n" + str(dictionary))
     
 load_dict()
 
@@ -46,7 +43,6 @@ while True:
     else:
         play_again = str(input("Find Another Word? [Y/N]: ")).lower()
         if play_again == "y":
-            potential_word.clear()
             dictionary.clear()
             load_dict()
             continue
