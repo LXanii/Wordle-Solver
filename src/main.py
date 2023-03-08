627288f1a884df082fc5a561c2b51b2601c23383
Xanii <73562093+LXanii@users.noreply.github.com>
2023-03-08 13:11:43 -0500

    Final Version (v1.0)

--- src/main.py
+++ src/main.py
@@ -13,11 +13,34 @@ def load_dict(): # made by joe | loads the dictionary to a list
             dictionary.append(i.lower()) # adds the word to the dictionary array
     print("Finished loading Dictionary\n" + str(len(dictionary)), "words found.")
 
+def retrieve_letters():
+    print('\nPut greys into input statment as periods. [Ex. Steak, .t.ak]') # made by kosta & joe
+    while True:
+        green_letters = str(input('\nLetter Formation [Green Letters]: \n')).lower()
+        for i in green_letters:
+            temp_letters.append(i)
+            if "." not in i:
+                confirmed_letters.append(i)
+        look_for_word(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
+        temp_letters.clear()
+        yellow_letters = str(input('\nLetter Formation [Yellow Letters]: \n')).lower()
+        for i in yellow_letters:
+            temp_letters.append(i)
+            if "." not in i:
+                if i not in confirmed_letters:
+                    confirmed_letters.append(i)
+        look_for_yellow(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
+        temp_letters.clear()
+        gay_letters = str(input("\nLetter Formation [Gray Letters]: \n")).lower()
+        for i in gay_letters:
+            temp_letters.append(i)
+        look_for_gay(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
+        print("\nPredicted Words\n" + str(dictionary))
+        temp_letters.clear()
+
 def look_for_word(a, b, c, d, e): # made by joe | gets the index values of the letters
     matching.clear()
     look_for = a + b + c + d + e # adds the letters together
-    print("This may take a little...")
-    print("Checking for", look_for)
     for i in range(1, len(look_for) + 1): # loops for the amount of indexes
         if look_for[i-1] != ".": # checks if the placement if valid
             for p in dictionary:
@@ -38,15 +61,15 @@ def look_for_yellow(a, b, c, d, e):#made by kosta
                         matching.append(p)
                 dictionary.clear()
                 for m in matching:
-                    dictionary.append(m) 
-                matching.clear()                
+                    dictionary.append(m)
+                matching.clear()
 
 def look_for_gay(a, b, c, d, e): #made by joe
     matching.clear()
     look_for = a + b + c + d + e # adds the letters together
     for i in range(1, len(look_for) + 1): # loops for the amount of indexes
         if look_for[i-1] != ".": # checks if the placement if valid
-            if look_for[i-1] not in confirmed_letters:
+            if look_for[i-1] not in confirmed_letters: # checks through the array confirmed letters
                 for p in dictionary:
                     if look_for[i-1] not in p:
                         matching.append(p)
@@ -56,27 +79,4 @@ def look_for_gay(a, b, c, d, e): #made by joe
                 matching.clear()
 
 load_dict()
-
-print('\nPut greys into input statment as periods. [Ex. Steak, .t.ak]')#made by kosta & joe
-while True:
-    green_letters = str(input('\nLetter Formation [Green Letters]: \n')).lower()
-    for i in green_letters:
-        temp_letters.append(i)
-        if "." not in i:
-            confirmed_letters.append(i)
-    look_for_word(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
-    temp_letters.clear()
-    yellow_letters = str(input('\nLetter Formation [Yellow Letters]: \n')).lower()
-    for i in yellow_letters:
-        temp_letters.append(i)
-        if "." not in i:
-            confirmed_letters.append(i)
-    look_for_yellow(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
-    temp_letters.clear()
-        
-    gay_letters = str(input("\nLetter Formation [Gray Letters]: \n")).lower()
-    for i in gay_letters:
-        temp_letters.append(i)
-    look_for_gay(temp_letters[0], temp_letters[1], temp_letters[2], temp_letters[3], temp_letters[4])
-    print("\nPredicted Word|s\n" + str(dictionary))
-    temp_letters.clear()
+retrieve_letters()
